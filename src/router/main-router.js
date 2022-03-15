f3f2cef87ff5b9a94b50452187f9ab9a603fff41
Redstone-1 <1825744594@qq.com>
2022-03-15 22:18:43 +0800

    feat: 完成路由分配用户角色功能，不同用户角色能看到不同的菜单，并增加了路由切换进度显示功能

--- src/router/main-router.js
+++ src/router/main-router.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import Home from '../views/NewsSandbox/Home/Home';
 import UserList from '../views/NewsSandbox/UserManage/UserList';
@@ -13,6 +13,8 @@ import AuditList from '../views/NewsSandbox/AuditList/AuditList'
 import Unpublish from '../views/NewsSandbox/Unpublish/Unpublish'
 import Publish from '../views/NewsSandbox/Publish/Publish'
 import HasOffLine from '../views/NewsSandbox/HasOffLine/HasOffLine'
+
+import { $get } from '../api/request';
 
 const localRouterMap = {
   "/home": Home,
@@ -30,12 +32,50 @@ const localRouterMap = {
 }
 
 export default function MainRouter(props) {
+  const [routesList, setRoutesList] = useState([])
+  const { role: {rights} } = JSON.parse(localStorage.getItem("token"))
+
+  useEffect(() => {
+    Promise.all([
+      $get("/rights"),
+      $get("/children")
+    ]).then(res => {
+      if (res && res.length > 0) {
+        console.log("res", res);
+        setRoutesList([...res[0].data, ...res[1].data])
+      }
+    })
+  }, [])
+
+
+  const checkRoute = (item) => {
+    return localRouterMap[item.key] === item.pagepermission
+  }
+
+  const checkUserPermission = (item) => {
+    return rights.includes(item.key)
+  }
+  
   return (
     <Switch>
-      <Route path="/home" component={Home} />
+      {
+        routesList.map(item => {
+          if (checkRoute(item) && checkUserPermission(item)) {
+            return <Route 
+              exact 
+              path={item.key} 
+              key={item.key} 
+              component={localRouterMap[item.key]} 
+            />
+          } else {
+            return null
+          }
+        })
+      }
+      {/* <Route path="/home" component={Home} />
       <Route path="/user-manage/list" component={UserList} />
       <Route path="/right-manage/role/list" component={RoleList} />
-      <Route path="/right-manage/right/list" component={RightList} />
+      <Route path="/right-manage/right/list" component={RightList} /> */}
       <Redirect from="/" to="/home" exact />
       <Route path="*" component={NotPermission} />
     </Switch>
--- src/views/NewsSandbox/newsSandbox.js
+++ src/views/NewsSandbox/newsSandbox.js
@@ -1,32 +1,40 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import TopHeader from '../../components/Sandbox/TopHeader';
 import SideMenu from '../../components/Sandbox/SideMenu';
 import { Layout } from 'antd';
 import MainRouter from '../../router/main-router';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
 import "./NewsSandbox.css";
 
-export default class NewsSandbox extends Component {
-  render() {
-    const { Content } = Layout
-    return (
-      <Layout>
-        <SideMenu></SideMenu>
-        <Layout className="site-layout">
-          <TopHeader></TopHeader>
-          <Content
-            style={{
-              margin: '16px 16px',
-              padding: 16,
-              minHeight: 280,
-              backgroundColor: "#fff",
-              overflow: "auto"
-            }}
-          >
-            <MainRouter />
-          </Content>
-        </Layout>
+const { Content } = Layout
+
+export default function NewsSandbox() {
+  
+  NProgress.start()
+
+  useEffect(() => {
+    NProgress.done()
+  })
+  
+  return (
+    <Layout>
+      <SideMenu></SideMenu>
+      <Layout className="site-layout">
+        <TopHeader></TopHeader>
+        <Content
+          style={{
+            margin: '16px 16px',
+            padding: 16,
+            minHeight: 280,
+            backgroundColor: "#fff",
+            overflow: "auto"
+          }}
+        >
+          <MainRouter />
+        </Content>
       </Layout>
-    )
-  }
+    </Layout>
+  )
 }
 
